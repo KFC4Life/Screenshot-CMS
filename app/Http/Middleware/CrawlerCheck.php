52afeaee599a7781c2eaa89349e9af53b74d7f86
Siemen Rotensen <info@siemenrotensen.nl>
2018-01-01 21:59:33 +0100

    Added crawler-detect plugin

--- app/Http/Middleware/CrawlerCheck.php
+++ app/Http/Middleware/CrawlerCheck.php
@@ -3,7 +3,6 @@
 namespace App\Http\Middleware;
 
 use App\Models\CrawlStatus;
-use Closure;
 
 class CrawlerCheck
 {
@@ -16,33 +15,29 @@ class CrawlerCheck
      */
     public function handle($request, Closure $next)
     {
-        $user_agent = $request->header('User-Agent');
 
-        switch($user_agent) {
-            case 'Mozilla/5.0 (compatible; Discordbot/2.0; +https://discordapp.com)':
-                // Discord
-                $crawl = new CrawlStatus();
-                $crawl->screenshot_id = $request->path();
-                $crawl->platform = 'discord';
-                $crawl->save();
-                break;
-            case 'Mozilla/5.0 (Windows NT 6.1; WOW64) SkypeUriPreview Preview/0.5':
-                // SkypeUriPreview
-                $crawl = new CrawlStatus();
-                $crawl->screenshot_id = $request->path();
-                $crawl->platform = 'skype';
-                $crawl->save();
-                break;
-            case 'Slack-ImgProxy 0.19 (+https://api.slack.com/robots)':
-                // Slack
-                $crawl = new CrawlStatus();
-                $crawl->screenshot_id = $request->path();
-                $crawl->platform = 'slack';
-                $crawl->save();
-                break;
-            default:
-                // Do nothing
-                break;
+        // Discord
+        if(Crawler::isCrawler('Mozilla/5.0 (compatible; Discordbot/2.0; +https://discordapp.com)')) {
+            $crawl = new CrawlStatus();
+            $crawl->screenshot_id = $request->path();
+            $crawl->platform = 'discord';
+            $crawl->save();
+        }
+
+        // Skype
+        if(Crawler::isCrawler('Mozilla/5.0 (Windows NT 6.1; WOW64) SkypeUriPreview Preview/0.5')) {
+            $crawl = new CrawlStatus();
+            $crawl->screenshot_id = $request->path();
+            $crawl->platform = 'skype';
+            $crawl->save();
+        }
+
+        // Slack
+        if(Crawler::isCrawler('Slack-ImgProxy 0.19 (+https://api.slack.com/robots)')) {
+            $crawl = new CrawlStatus();
+            $crawl->screenshot_id = $request->path();
+            $crawl->platform = 'slack';
+            $crawl->save();
         }
 
         return $next($request);
--- config/app.php
+++ config/app.php
@@ -227,6 +227,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Crawler' => Jaybizzle\LaravelCrawlerDetect\Facades\LaravelCrawlerDetect::class,
+
 
     ],
 
