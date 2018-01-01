a391053e5ae0172c3dcb76e041c9ffd71a5c77e4
Siemen Rotensen <info@siemenrotensen.nl>
2018-01-01 22:47:28 +0100

    Fix this later

--- app/Http/Middleware/CrawlerCheck.php
+++ app/Http/Middleware/CrawlerCheck.php
@@ -17,29 +17,29 @@ class CrawlerCheck
     public function handle($request, Closure $next)
     {
 
-        // Discord
-        if(Crawler::isCrawler('Mozilla/5.0 (compatible; Discordbot/2.0; +https://discordapp.com)')) {
-            $crawl = new CrawlStatus();
-            $crawl->screenshot_id = $request->path();
-            $crawl->platform = 'discord';
-            $crawl->save();
-        }
-
-        // Skype
-        if(Crawler::isCrawler('Mozilla/5.0 (Windows NT 6.1; WOW64) SkypeUriPreview Preview/0.5')) {
-            $crawl = new CrawlStatus();
-            $crawl->screenshot_id = $request->path();
-            $crawl->platform = 'skype';
-            $crawl->save();
-        }
-
-        // Slack
-        if(Crawler::isCrawler('Slack-ImgProxy 0.19 (+https://api.slack.com/robots)')) {
-            $crawl = new CrawlStatus();
-            $crawl->screenshot_id = $request->path();
-            $crawl->platform = 'slack';
-            $crawl->save();
-        }
+//        // Discord
+//        if(Crawler::isCrawler('Mozilla/5.0 (compatible; Discordbot/2.0; +https://discordapp.com)')) {
+//            $crawl = new CrawlStatus();
+//            $crawl->screenshot_id = $request->path();
+//            $crawl->platform = 'discord';
+//            $crawl->save();
+//        }
+//
+//        // Skype
+//        if(Crawler::isCrawler('Mozilla/5.0 (Windows NT 6.1; WOW64) SkypeUriPreview Preview/0.5')) {
+//            $crawl = new CrawlStatus();
+//            $crawl->screenshot_id = $request->path();
+//            $crawl->platform = 'skype';
+//            $crawl->save();
+//        }
+//
+//        // Slack
+//        if(Crawler::isCrawler('Slack-ImgProxy 0.19 (+https://api.slack.com/robots)')) {
+//            $crawl = new CrawlStatus();
+//            $crawl->screenshot_id = $request->path();
+//            $crawl->platform = 'slack';
+//            $crawl->save();
+//        }
 
         return $next($request);
     }
