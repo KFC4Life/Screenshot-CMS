4fc26d46aef1a7dad21ebd08f57be81a74b31679
Siemen Rotensen <info@siemenrotensen.nl>
2018-05-07 17:50:48 +0200

    Make logbook table responsive

--- resources/views/logbook.blade.php
+++ resources/views/logbook.blade.php
@@ -15,26 +15,28 @@
             <hr />
         </div>
 
-        <table class="table table-dark table-striped">
-            <thead class="thead-dark">
-                <tr>
-                    <th scope="col">Event</th>
-                    <th scope="col">Info</th>
-                    <th scope="col">IP Address</th>
-                    <th scope="col">Time</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($log as $line)
-                <tr @if($line->event == 'upload') class="bg-success" @elseif($line->event == 'delete') class="bg-warning" @elseif($line->event == 'restore') class="bg-primary" @elseif($line->event == 'permanently-delete') class="bg-danger" @else class="bg-secondary" @endif>
-                    <th>{{ $line->event }}</th>
-                    <td>{{ $line->info }}</td>
-                    <td>{{ $line->ip }}</td>
-                    <td>{{ $line->created_at }}</td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
+        <div class="table-responsive">
+            <table class="table table-dark table-striped">
+                <thead class="thead-dark">
+                    <tr>
+                        <th scope="col">Event</th>
+                        <th scope="col">Info</th>
+                        <th scope="col">IP Address</th>
+                        <th scope="col">Time</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($log as $line)
+                    <tr @if($line->event == 'upload') class="bg-success" @elseif($line->event == 'delete') class="bg-warning" @elseif($line->event == 'restore') class="bg-primary" @elseif($line->event == 'permanently-delete') class="bg-danger" @else class="bg-secondary" @endif>
+                        <th>{{ $line->event }}</th>
+                        <td>{{ $line->info }}</td>
+                        <td>{{ $line->ip }}</td>
+                        <td>{{ $line->created_at }}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
 
         {{ $log->links() }}
         @else
