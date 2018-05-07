b9da0453eb1ceb2ed9133e3f171a9e7f2ac0ea14
Siemen Rotensen <info@siemenrotensen.nl>
2018-05-07 19:09:54 +0200

    Fix for cloudflare ip + removed unnecessary things

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,14 +18,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (Schema::hasTable('settings')) {
-            View::share('screenshots_count', count(DB::table('screenshots')->get()));
-        }
-        
-        if (isset($_SERVER["HTTP_CF_CONNECTING_IP"])) {
-            $_SERVER['REMOTE_ADDR'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
-        }
-
         if(env('REDIRECT_HTTPS'))
         {
             URL::forceScheme('https');
